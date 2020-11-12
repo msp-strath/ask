aae4011858ece1e290bd8936776a0cfec7142bec
Conor McBride <conor@strictlypositive.org>
2020-11-12 17:14:16 +0000

    deduction checking is go

---
+++ src/Thin.hs
@@ -0,0 +1,58 @@
+module Thin where
+
+import Data.Bits
+
+newtype Thinning = Th Integer
+
+instance Show Thinning where
+  show (Th th) = go th where
+    go 0 = "...0"
+    go (-1) = "...1"
+    go th = case unsnocBit th of
+      (th, b) -> go th ++ if b then "1" else "0"
+
+os :: Thinning -> Thinning
+os (Th th) = Th (snocBit th True)
+
+o' :: Thinning -> Thinning
+o' (Th th) = Th (snocBit th False)
+
+snocBit :: Bits a => a -> Bool -> a
+snocBit bs True = shiftL bs 1 .|. (bit 0)
+snocBit bs False = shiftL bs 1
+
+unsnocBit :: Bits a => a -> (a, Bool)
+unsnocBit bs = (shiftR bs 1, testBit bs 0)
+
+instance Semigroup Thinning where
+  (<>) = mappend
+
+instance Monoid Thinning where
+  mempty = Th (-1)
+  Th th `mappend` Th ph = Th (go th ph) where
+    go th 0    =  0
+    go th (-1) =  th
+    go th ph = case unsnocBit ph of
+      (ph, False) -> snocBit (go th ph) False
+      (ph, True)  -> case unsnocBit th of
+        (th, b) -> snocBit (go th ph) b
+
+class Thin t where
+  (<^>) :: t -> Thinning -> t
+  thicken :: Thinning -> t -> Maybe t
+
+instance Thin Int where
+  i <^> Th th = case (th `mod` 2, th `div` 2) of
+    (0, th) -> 1 + (i <^> Th th)
+    (1, th) -> case i of
+      0 -> 0
+      _ -> 1 + ((i - 1) <^> Th th)
+  thicken (Th th) i = go th i where
+    go 0 i = Nothing
+    go (-1) i = Just i
+    go th i = case (unsnocBit th, i) of
+      ((th, False), 0) -> Nothing
+      ((th, False), i) -> go th (i-1)
+      ((th, True), 0) -> Just 0
+      ((th, True), i) -> (1+) <$> go th (i-1)
+
